48966e2a18567b48a48f789714470bbacb90d353
matias <matias.stornini00@gmail.com>
2022-05-01 22:39:09 -0300

    ss

--- conf.js
+++ conf.js
@@ -1,10 +1,9 @@
 window.appConf = {
     author: 'React Drive CMS',
-    dashboardId: '1-on_GfmvaEcOk7HcWfKb8B6KFRv166RkLN2YmDEtDn4',
+    dashboardId: '1uCOGRDfoEecgaJLbWwiE2ToFhUweHf-uX4g0dGyVwIA',
     sendContactMessageUrlId:
         'AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d',
     shortname: 'easydrivecms',
-    root: 'react-drive-cms',
     ipInfoToken: '6efb7a808a4857',
-    googleApiKey: 'AIzaSyBYOwC55SpcCZaG9d87UuHkxkQ8GRI_39M',
+    googleApiKey: 'AIzaSyCxFwdfal7E5W-LJUUCjjJ4PY0KHNcY_m0',
 }

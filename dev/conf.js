cdd136ddcf893d6ae97d42ea0522e6238f7796fc
matias <matias.stornini00@gmail.com>
2022-05-01 22:34:31 -0300

    ss

--- dev/conf.js
+++ dev/conf.js
@@ -7,12 +7,3 @@ window.appConf = {
     ipInfoToken: '6efb7a808a4857',
     googleApiKey: 'AIzaSyCxFwdfal7E5W-LJUUCjjJ4PY0KHNcY_m0',
 }
-window.appConf = {
-    author: 'React Drive CMS',
-    dashboardId: '1uCOGRDfoEecgaJLbWwiE2ToFhUweHf-uX4g0dGyVwIA',
-    sendContactMessageUrlId:
-        'AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d',
-    shortname: 'easydrivecms',
-    ipInfoToken: '6efb7a808a4857',
-    googleApiKey: 'AIzaSyCxFwdfal7E5W-LJUUCjjJ4PY0KHNcY_m0',
-}
